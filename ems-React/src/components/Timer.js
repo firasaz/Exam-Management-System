374b55c7c4eb1e56155757f605e33035d595002c
Abdullah Sherif <asmnpis@gmail.com>
2023-01-23 19:59:54 +0200

    Timer is added

---
+++ ems-React/src/components/Timer.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+function Timer() {
+  const [time, setTime] = useState({ minutes: 30, seconds: 0 });
+  const [intervalId, setIntervalId] = useState(null);
+  const [isFinished, setIsFinished] = useState(false);
+
+  useEffect(() => {
+    const newIntervalId = setInterval(() => {
+      if (time.seconds > 0) {
+        setTime((time) => ({ ...time, seconds: time.seconds - 1 }));
+      } else if (time.minutes === 0 && time.seconds === 0) {
+        clearInterval(newIntervalId);
+        setIsFinished(true);
+      } else {
+        setTime((time) => ({
+          ...time,
+          minutes: time.minutes - 1,
+          seconds: 59,
+        }));
+      }
+    }, 1000);
+    setIntervalId(newIntervalId);
+    return () => clearInterval(newIntervalId);
+  }, [time]);
+
+  useEffect(() => {
+    setTime({ minutes: 30, seconds: 0 });
+  }, []);
+
+  return (
+    <div>
+      <h1>
+        <center>
+          Timer: {time.minutes}:
+          {time.seconds < 10 ? `0${time.seconds}` : time.seconds}
+        </center>
+      </h1>
+      {isFinished && (
+        <strong>
+          <div>Time is finished!</div>
+        </strong>
+      )}
+    </div>
+  );
+}
+
+export default Timer;
--- ems-React/src/components/User/TakeQuiz.js
+++ ems-React/src/components/User/TakeQuiz.js
@@ -1,35 +1,25 @@
+
 import Sidebar from "./Sidebar";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Timer from "../Timer"; // import the Timer component
+
 const baseUrl = "http://127.0.0.1:8000/api";
+
 function TakeQuiz() {
   const [questionData, setquestionData] = useState([]);
   const { quiz_id } = useParams();
   const studentId = localStorage.getItem("studentId");
 
-  // useEffect(() => {
-  //   try {
-  //     axios.get(`${baseUrl}/quiz-questions/${quiz_id}/1/`).then((res) => {
-  //       console.log(res.data);
-  //       setquestionData(res.data);
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  //   document.title = "Attempt Exam";
-  // }, []);
-
   useEffect(() => {
     try {
-      axios.get(`${baseUrl}/exam-questions/${quiz_id}/`).then((res) => {
-        // console.log(res.data);
+      axios.get(baseUrl + "/quiz-questions/" + quiz_id + "/1").then((res) => {
         setquestionData(res.data);
       });
     } catch (error) {
       console.log(error);
     }
-    document.title = "Attempt Exam";
   }, []);
 
   const submitAnswer = (question_id, right_ans) => {
@@ -38,10 +28,9 @@ function TakeQuiz() {
     _formData.append("quiz", quiz_id);
     _formData.append("question", question_id);
     _formData.append("right_ans", right_ans);
-
     try {
       axios
-        .post(`${baseUrl}/attempt-quiz/`, _formData, {
+        .post(baseUrl + "/attempt-quiz/", _formData, {
           headers: {
             "content-type": "multipart/form-data",
           },
@@ -51,7 +40,11 @@ function TakeQuiz() {
             try {
               axios
                 .get(
-                  `${baseUrl}/quiz-questions/${quiz_id}/next-question/${question_id}/`
+                  baseUrl +
+                    "/quiz-questions/" +
+                    quiz_id +
+                    "/next-question/" +
+                    question_id
                 )
                 .then((res) => {
                   setquestionData(res.data);
@@ -73,77 +66,65 @@ function TakeQuiz() {
           <Sidebar />
         </aside>
         <section className="col-md-9">
-        <h4 className="mb-3 border-bottom pb-1">
-                Exam: <span className="text-danger">{questionData[0]?.exam}</span>
-        </h4>
+          <Timer minutes={1} />
           {questionData.map((row, index) => (
-              <div className="card" key={index}>
-                <h5 className="card-header">{row?.question}</h5>
+            <>
+              <h4 className="mb-3 border-bottom pb-1">
+                Exam: <span className="text-danger">{row.quiz.title}</span>
+              </h4>
+              <div className="card">
+                <h5 className="card-header">{row.questions}</h5>
                 <div className="card-body">
                   <table className="table table-bordered">
                     <tbody>
-                      {/* {console.log("row_answers:",row?.answers[1])} */}
-                      {row.answers.map((ans,index2) => {
-                        return(
-                        <tr key={index2}>
+                      <>
+                        <tr>
                           <td>
                             <button
-                              onClick = {() => submitAnswer(ans?.id, ans?.text)}
-                              className = "btn btn-outline-secondary">
-                                {ans.text}
+                              onClick={() => submitAnswer(row.id, row.ans1)}
+                              className="btn btn-outline-secondary"
+                            >
+                              {row.ans1}
                             </button>
                           </td>
-                        </tr>)
-                      })}
-
-                      {/* <tr>
-                        <td>
-                          <button
-                            onClick={() => submitAnswer(row?.answers?.id, row?.answers?.text)}
-                            className="btn btn-outline-secondary"
-                          >
-                            {row?.answers?.text}
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <button
-                            onClick={() => submitAnswer(row?.id, row?.ans2)}
-                            className="btn btn-outline-secondary"
-                          >
-                            {row?.ans2}
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <button
-                            onClick={() => submitAnswer(row?.id, row?.ans3)}
-                            className="btn btn-outline-secondary"
-                          >
-                            {row?.ans3}
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <button
-                            onClick={() => submitAnswer(row?.id, row?.ans4)}
-                            className="btn btn-outline-secondary"
-                          >
-                            {row?.ans4}
-                          </button>
-                        </td>
-                      </tr> */}
-                  </tbody>
-                </table>
+                        </tr>
+                        <tr>
+                          <td>
+                            <button
+                              onClick={() => submitAnswer(row.id, row.ans2)}
+                              className="btn btn-outline-secondary"
+                            >
+                              {row.ans2}
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <button
+                              onClick={() => submitAnswer(row.id, row.ans3)}
+                              className="btn btn-outline-secondary"
+                            >
+                              {row.ans3}
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <button
+                              onClick={() => submitAnswer(row.id, row.ans4)}
+                              className="btn btn-outline-secondary"
+                            >
+                              {row.ans4}
+                            </button>
+                          </td>
+                        </tr>
+                      </>
+                    </tbody>
+                  </table>
+                </div>
               </div>
-            </div>
+            </>
           ))}
-          {questionData.length === 0 && (
-            <span className="text-primary">No more questions</span>
-          )}
         </section>
       </div>
     </div>
